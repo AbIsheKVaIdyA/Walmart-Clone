39e89dd9ac244f7e0cf525167bf0c9ea19dd3e72
Abhishek Vaidya <abhishekvaidya.work@gmail.com>
2024-03-27 23:29:27 +0530

    day2 updates

---
+++ app/search/page.tsx
@@ -0,0 +1,18 @@
+import fetchSearch from "@/lib/fetchSearch";
+
+type Props = {
+    searchParams:{
+        q:string;
+    };
+}
+
+async function SearchPage({searchParams:{q}}:Props) {
+    const results = await fetchSearch(q);
+
+    console.log(results);
+    
+  return <div>page</div>;
+  
+}
+
+export default SearchPage
